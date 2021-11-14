2a1e95971a43975e94b60cf9372b6427c725ba32
ljh <185303694@qq.com>
2021-11-14 13:27:07 +0800

    add pretrained_model_predict_2.py to repository

---
+++ Backend/pretrained_model_predict_2.py
@@ -0,0 +1,20 @@
+# !pip install pysentimiento
+from pysentimiento import SentimentAnalyzer
+def pretrained_model_predict(text):
+    analyzer = SentimentAnalyzer(lang="es")
+    result = analyzer.predict(text)
+    #scores = [score for score in result]
+    print(result)
+    scores = result.probas
+    print(scores)
+    if scores['POS'] > 0.65:
+        return 4
+    elif scores['POS'] <= 0.65 and scores['POS'] > 0.4:
+        return 3
+    elif scores['NEG'] > 0.65:
+        return 0
+    elif scores['NEG'] <= 0.65 and scores['NEG'] > 0.4:
+        return 1
+    else:
+        return 2
+#print(pretrained_model_predict('i do not know.'))
